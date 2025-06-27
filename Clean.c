3e120689d9b9e4a3ac0ba6267ef1042798d9b301
EduardoBougo <eduardobougo@gmail.com>
2025-06-27 00:22:23 -0300

    Adição de novas funcionalidades
    Checar a função servicoMaisUsado, pois aparentemente não esta funcionando ou está com mal funcionamento.

--- Clean.c
+++ Clean.c
@@ -50,6 +50,10 @@ void cadastrarCliente(void);
 void cadastrarCarro(void);
 void buscarClientePeloNome(char nome[50]);
 void buscarCarroPelaPlaca(char placa[15]);
+int kilometragemCarros(float km);
+void agendarServico(void);
+void valoresPendentes(void);
+void servicoMaisUsado(void);
 
 int validarNome(char nome[50]);
 int contemSomenteLetras(char text[50]);
@@ -57,18 +61,15 @@ int contemSomenteLetras(char text[50]);
 int validarTelefone(char telefone[15]);
 int contemSomenteNumeros(char text[15]);
 
-int validarAnoFabricacao(int ano);
 int validarData(Data dt);
-int validarDataAnterior(Data dt);
 int anoBissexto(int ano);
+int validarDataAnterior(Data dt);
+int validarAnoFabricacao(int ano);
 
 int VincularCliente(char nomeCliente[50]);
 int VincularCarro(char modeloCarro[50], char placaCarro[15]);
 
-int kilometragemCarros(float km);
-
 void agruparPorAno(void);
-void agendarServico(void);
 
 void bancoDeDados(void);
 void linha(void);
@@ -149,7 +150,8 @@ void menuInterno(void){
     printf(" | 4 - Buscar Carro pela Placa                     |\n");
     printf(" | 5 - Quantidade de Carros com Kilometragem Acima |\n");
     printf(" | 6 - Agendar serviço                             |\n");
-    printf(" | 7 - Mostrar o serviço mais solicitado (quebrada)|\n");
+    printf(" | 7 - Consultar valores pendentes                 |\n");
+    printf(" | 8 - Mostrar o serviço mais solicitado           |\n");
     printf(" | 0 - Sair                                        |\n");
     printf(" |-------------------------------------------------|\n");
     printf(" Escolha uma opcao: ");
@@ -246,6 +248,25 @@ void menu(void){
             getchar();
 
             enterParaContinuar();
+            break;
+
+        case 7:
+            limparTela();
+            printf(" |-------------------------------------------------|\n");
+            printf(" |           CONSULTAR VALORES PENDENTES           |\n");
+            printf(" |-------------------------------------------------|\n");
+            valoresPendentes();
+            enterParaContinuar();
+            break;
+
+        case 8:
+            limparTela();
+            printf(" |-------------------------------------------------|\n");
+            printf(" |             SERVIÇO MAIS SOLICITADO             |\n");
+            printf(" |-------------------------------------------------|\n");
+            servicoMaisUsado();
+            enterParaContinuar();
+            break;
 
         default:
             limparTela();
@@ -463,7 +484,7 @@ int validarDataAnterior(Data dt)
         return 0;
     }
 
-     if (dt.ano < 2025 || (dt.ano == 2025 && dt.mes < 6) || (dt.ano == 2025 && dt.mes == 6 && dt.dia < 30))
+    if (dt.ano < 2025 || (dt.ano == 2025 && dt.mes < 6) || (dt.ano == 2025 && dt.mes == 6 && dt.dia < 30))
     { // Testando se é anterior que a data de hoje
         return 0;
     }
@@ -562,9 +583,12 @@ void agruparPorAno() {
     int i = 0;
     int j = 0;
 
-    for (int i = 0; i < g_qtdCarros; i++) {
-        for (j = i+1; j < g_qtdCarros; j++) {
-            if (g_carros[i].anoFabricacao > g_carros[j].anoFabricacao) {
+    for (int i = 0; i < g_qtdCarros; i++) 
+    {
+        for (j = (i + 1); j < g_qtdCarros; j++) 
+        {
+            if (g_carros[i].anoFabricacao > g_carros[j].anoFabricacao) 
+            {
                 //Trocar os carros, ordenando por ano de fabricação
                 Carro temp = g_carros[i];
                 g_carros[i] = g_carros[j];
@@ -574,9 +598,11 @@ void agruparPorAno() {
     }
 
     int anoAtual = 0;
-    for (i = 0; i < g_qtdCarros; i++) {
+    for (i = 0; i < g_qtdCarros; i++) 
+    {
         //Se o ano de fabricação não for igual ao da variável anoAtual, essa variável irá receber outro ano.
-        if (g_carros[i].anoFabricacao != anoAtual) {
+        if (g_carros[i].anoFabricacao != anoAtual) 
+        {
             printf("%d\n", g_carros[i].anoFabricacao);
             anoAtual = g_carros[i].anoFabricacao;
         }
@@ -592,13 +618,14 @@ void agruparPorAno() {
 }
 
 
-void agendarServico(void) {
+void agendarServico()
+{
     int validar = 0;
     int tipo = 0;
     char modelo[50];
     char placa[15];
     Data tempData;
-    
+
     do
     {
         printf("Digite o modelo do carro que deseja agendar o serviço: ");
@@ -607,13 +634,14 @@ void agendarServico(void) {
         printf("Digite a placa do carro: ");
         fgets(placa, 15, stdin);
         placa[strlen(placa) - 1] = '\0';
-        
+
         validar = VincularCarro(modelo, placa);
-        if(validar == -1) {
+        if (validar == -1)
+        {
             printf("Carro nao encontrado.\n");
         }
     } while (validar == -1);
-    
+
     g_servicos[g_qtdServicos].carro = g_carros[VincularCarro(modelo, placa)];
     linha();
 
@@ -630,9 +658,9 @@ void agendarServico(void) {
         {
             printf("Data invalida\n");
         }
-        
+
     } while (!validarDataAnterior(tempData));
-    
+
     g_servicos[g_qtdServicos].dataDoServico = tempData;
     linha();
 
@@ -644,14 +672,15 @@ void agendarServico(void) {
         printf("3 - Alinhamento e balanceamento - R$120.00\n");
         printf("4 - Higienizacao - R$90.00\n");
         scanf("%d", &tipo);
+        getchar();
 
         if (tipo != 1 && tipo != 2 && tipo != 3 && tipo != 4)
         {
             printf("Tipo invalido.\n");
         }
-        
+
     } while (tipo != 1 && tipo != 2 && tipo != 3 && tipo != 4);
-    
+
     g_servicos[g_qtdServicos].tipoDeServico = tipo;
 
     g_servicos[g_qtdServicos].pago = 'N';
@@ -659,8 +688,105 @@ void agendarServico(void) {
     g_servicos[g_qtdServicos].identificador = g_id;
     g_id++;
 
+    g_qtdServicos++;
+
     printf("Servico agendado!\n");
     linha();
+}
+
+void valoresPendentes(void)
+{
+    int i = 0;
+    int j = 0;
+    int encontrouServico = 0;
+    for (i = 0; i < g_qtdCarros; i++)
+    {
+        Carro carroAtual = g_carros[i];
+        printf("Carro: %s | Placa %s\n", carroAtual.modelo, carroAtual.placa);
+        encontrouServico = 0;
+
+        for (j = 0; j < g_qtdServicos; j++)
+        {
+            if (strcmp(carroAtual.placa, g_servicos[j].carro.placa) == 0)
+            {
+                encontrouServico = 1;
+                if (g_servicos[j].pago == 'N')
+                {
+                    int tipo = g_servicos[j].tipoDeServico; 
+                    switch(tipo) {
+                        case 1:
+                            printf("Serviço de Resisão básica (750,0) - não pago\n");
+                            break;
+                        case 2:
+                            printf("Serviço de Troca de óleo (190,0) - não pago\n");
+                            break;
+                        case 3:
+                            printf("Serviço de Alinhamento e balanceamento (120,0) - não pago\n");
+                            break;
+                        case 4:
+                            printf("Serviço de Higienização (90,0) - não pago\n");
+                            break;
+                    }
+                } 
+
+            }
+        }
+
+        if (!encontrouServico)
+        {
+            printf("Não possui servico no momento.\n");
+        }
+        
+        linha();
+    }
+}
+
+void servicoMaisUsado(void) {
+    int i = 0;
+    int j = 0;
+    int num = 0;
+    int vezes = 0;
+    int maior = 0;
+
+    for (i = 0; i < g_qtdServicos; i++)
+    {
+        vezes = 0;
+        for (j = (i + 1); j < g_qtdServicos; j++)
+        {
+            if (g_servicos[i].tipoDeServico == g_servicos[j].tipoDeServico)
+            {
+                vezes++;
+            }
+            
+        }
+        
+        if (vezes > maior)
+        {
+            maior = vezes;
+            num = g_servicos[i].tipoDeServico;
+        }
+        
+    }
+    
+    printf("Serviço mais pedido: ");
+
+    switch (num)
+    {
+        case 1:
+            printf("Revisão básica");
+            break;
+        case 2:
+            printf("Troca de óleo");
+            break;
+        case 3:
+            printf("Alinhamento e balanceamento");
+            break;
+        case 4:
+            printf("Higienização");
+            break;
+        default:
+            break;
+    }
 }
 
 /**********************************************
